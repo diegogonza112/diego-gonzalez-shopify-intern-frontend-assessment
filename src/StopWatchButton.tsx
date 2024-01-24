8301009f5f1eb935033aed7cc1cf1a425bb78fba
diego <diego.gonzalezdelgado@mail.utoronto.ca>
2024-01-24 13:28:47 -0500

    basic stop watch functs ++ updated button

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,12 +1,13 @@
 import React, { MouseEventHandler } from 'react'
 
-interface StopWatchButtonInterface { onClick: MouseEventHandler, text: string }
+interface StopWatchButtonInterface { onClick: MouseEventHandler, text: string, disabled: boolean }
 
 export default function StopWatchButton(props: StopWatchButtonInterface) {
 
     return(
         <button
             onClick={props.onClick}
+            disabled={props.disabled}
             >
             {props.text}
         </button>
